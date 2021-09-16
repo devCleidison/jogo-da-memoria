a7857b286d2995fa56d2084196a1595a61b1a84f
Cleidison Silva <cleidison.dev@gmail.com>
2021-09-15 21:51:24 -0300

    JS - cartas em posições aleatórias

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+const FRONT = "card_front";
+const BACK = "card_back";
+
+let techs = [
+  "bootstrap",
+  "css",
+  "electron",
+  "firebase",
+  "html",
+  "javascript",
+  "jquery",
+  "mongo",
+  "node",
+  "react",
+];
+
+let cards = null;
+
+startGame();
+
+function startGame() {
+  cards = createCardsFromTechs(techs);
+  shuffleCards(cards);
+
+  console.log(cards)
+}
+
+function shuffleCards(cards) {
+  let currentIndex = cards.length;
+  let randomIndex = 0;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+  }
+}
+
+function createCardsFromTechs(techs) {
+  let cards = [];
+
+  for (let tech of techs) {
+    cards.push(createPairFromTech(tech));
+  }
+
+  return cards.flatMap((pair) => pair);
+}
+
+function createPairFromTech(tech) {
+  return [
+    {
+      id: createIdWithTech(tech),
+      icon: tech,
+      flipped: false,
+    },
+    {
+      id: createIdWithTech(tech),
+      icon: tech,
+      flipped: false,
+    },
+  ];
+}
+
+function createIdWithTech(tech) {
+  return tech + parseInt(Math.random() * 1000);
+}

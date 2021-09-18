fd139692821365f70ed775414cd49785b316b907
Cleidison Silva <cleidison.dev@gmail.com>
2021-09-18 07:52:17 -0300

    Populando o tabuleiro com JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card";
+const ICON = 'icon'
 
 let techs = [
   "bootstrap",
@@ -22,7 +24,44 @@ function startGame() {
   cards = createCardsFromTechs(techs);
   shuffleCards(cards);
 
-  console.log(cards)
+  initializeCards(cards);
+}
+
+function initializeCards(cards) {
+  let gameBoard = document.getElementById("gameBoard");
+
+  cards.forEach((card) => {
+    let cardElement = document.createElement("div");
+    cardElement.id = card.id;
+    cardElement.classList.add(CARD);
+    cardElement.dataset.icon = card.icon;
+
+    createCardContent(card, cardElement);
+
+    cardElement.addEventListener("click", flipCard);
+
+    b = gameBoard.appendChild(cardElement);
+  });
+}
+
+function createCardContent(card, cardElement) {
+  createCardFace(FRONT, card, cardElement);
+  createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+  let cardElementFace = document.createElement("div");
+  cardElementFace.classList.add(face);
+
+  if (face === FRONT) {
+    let iconElement = document.createElement('img')
+    iconElement.classList.add(ICON)
+    iconElement.src = './assets/images/' + card.icon + '.png'
+    cardElementFace.appendChild(iconElement)
+  } else {
+    cardElementFace.innerHTML = "&lt/&gt";
+  }
+  element.appendChild(cardElementFace)
 }
 
 function shuffleCards(cards) {
@@ -33,16 +72,19 @@ function shuffleCards(cards) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
-    [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+    [cards[randomIndex], cards[currentIndex]] = [
+      cards[currentIndex],
+      cards[randomIndex],
+    ];
   }
 }
 
 function createCardsFromTechs(techs) {
   let cards = [];
 
-  for (let tech of techs) {
+  techs.forEach((tech) => {
     cards.push(createPairFromTech(tech));
-  }
+  });
 
   return cards.flatMap((pair) => pair);
 }
@@ -64,4 +106,8 @@ function createPairFromTech(tech) {
 
 function createIdWithTech(tech) {
   return tech + parseInt(Math.random() * 1000);
+}
+
+function flipCard() {
+  this.classList.add('flip')
 }
